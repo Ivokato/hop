656d6a8438e335230cd98acfc968d0ce4cee0e16
Ivo de Kler <ivo.de.kler@gmail.com>
2016-09-03 00:50:23 +0200

    background scrolls again after overlay closes
    always remove overlay with `$overlay.trigger('remove');`

--- public/js/basic.js
+++ public/js/basic.js
@@ -3,86 +3,17 @@ var lightboxFilmstripSize = {x: 100, y: 75};
 
 // transitions
 var pageTransitions = {
-			crossfade: function($oldContent, injectNew, removeOld){
-				$oldContent.transition({opacity: 0}, 200, 'ease', function(){
-					removeOld();
-					injectNew({opacity: 0}, function injectCallback($newContent, done){
-						$newContent.transition({opacity: 1}, 300, 'ease', function(){
-							done();
-						});
-					});
-				});
-			},
-			flip: function($oldContent, injectNew, removeOld){
-				$oldContent.transition({
-					rotateY: 90,
-					opacity: 0
-				}, 600, 'in', function onOldTransitionEnd(){
-					removeOld();
-					injectNew({
-						rotateY: -90,
-						opacity: 0
-					},
-					function injectCallback($newContent, done){
-						$newContent.transition({
-							rotateY: 0,
-							opacity: 1
-						}, 600, 'out', done);
-					});
-				});
-			},
-			rotaSwap: function($oldContent, injectNew, removeOld){
-				$oldContent
-				.css({'transform-origin': '50% 0%'})
-				.transition({
-					rotate: 180,
-					opacity: 0
-				}, 1500, 'in', function onOldTransitionEnd(){
-					removeOld();
-					injectNew({
-						rotate: -180,
-						opacity: 0,
-						'transform-origin': '50% 0%'
-					},
-					function injectCallback($newContent, done){
-						$newContent.transition({
-							rotate: 0,
-							opacity: 1
-						}, 1500, 'out', done);
-					});
-				});
-			},
-			shiftNext: function($oldContent, injectNew, removeOld){
-				injectNew({
-					'transform-origin': '100% 0%',
-					scale: 0,
-					opacity: 1
-				}, function injectCallback($newContent, done){
-
-					$oldContent
-					.css({
-						'transform-origin': '0% 0%',
-						position: 'absolute',
-						top: $oldContent.offset().top + 'px',
-						left: $oldContent.offset().left + 'px',
-						width: $oldContent.width() + 'px'
-					})
-					.transition({
-						scale: 0
-					}, 2000, 'linear', removeOld);
-
-					$newContent.transition({
-						scale: 1
-					}, 2000, 'linear', function(){
-						$newContent.css({
-							'transform-origin': 'initial'
-						});
-						done();
-					});
-
-				});
-			}
-		};
+	crossfade: function($oldContent, injectNew, removeOld){
+    $oldContent.transition({opacity: 0}, 200, 'ease', function(){
+      removeOld();
+      injectNew({opacity: 0}, function injectCallback($newContent, done){
+        $newContent.transition({opacity: 1}, 300, 'ease', function(){
+          done();
+        });
+      });
+    });
+  }
+};
 
 function pageTransition($oldContent, injectNew, removeOld, style){
 	if(style && pageTransitions[style]){
@@ -364,23 +295,29 @@ function pageTransition($oldContent, injectNew, removeOld, style){
 		document.body.classList.add( 'noscroll' );
 		
 		$(this).append($overlay);
+
+		function fadeAway(){
+			$overlay.animate({ opacity: 0 }, 200, function(){
+				$overlay.trigger( 'remove' );
+			});
+		}
 		
 		$overlay.on({
 			click: function(e){
 				if(e.target !== this) return;
-				$overlay.animate({ opacity: 0 }, 200, function(){
-					$overlay.trigger( 'remove' );
-					$overlay.remove();
-				});
+				fadeAway();
 			},
 			remove: function(){
 				document.body.classList.remove('noscroll');
+				$overlay.remove();
 			}
 		});
 		$overlay.animate({ opacity: 1 }, 500);
 		$(document).on('keyup.overlay', function(e){
 			if(e.keyCode === 27){
-				$overlay.remove();
+				$overlay.animate({ opacity: 0 }, 200, function(){
+					$overlay.trigger( 'remove' );
+				});
 				$(document).off('keyup.overlay');
 			}
 		});
@@ -701,7 +638,7 @@ function pageTransition($oldContent, injectNew, removeOld, style){
 
 $(document).ready(function(){
 	window.imageLoader.init();
-	$(document.body).trigger('pageTransitionEnd', [{ href: location.pathname }]);
+	$(document.body).trigger('pageTransitionEnd', [{ href: location.pathname }, 'test']);
 });
 
 if(window.localStorage){
